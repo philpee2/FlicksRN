d5f3800d304735f1300159c5215733b51bdd6342
Phil Nachum <phil.nachum@airbnb.com>
2016-08-24 09:55:22 -0700

    Fix poster images

--- EndpointConstants.js
+++ EndpointConstants.js
@@ -1,5 +1,5 @@
 export const NOW_PLAYING = 'now_playing';
 export const TOP_RATED = 'top_rated';
 export const API_KEY = 'a07e22bc18f5cb106bfe4cc1f83ad8ed';
-export const BASE_IMAGE_URL = 'http://image.tmdb.org/t/p/original';
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/original';
 export const BASE_DATA_URL = 'https://api.themoviedb.org/3/movie';
